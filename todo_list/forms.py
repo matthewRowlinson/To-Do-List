daa2670b0415d47bee7f0f891fc31b4e3bb00874
matthewRowlinson <60362896+matthewRowlinson@users.noreply.github.com>
2020-03-08 14:45:28 -0400

    Update forms.py

--- todo_list/forms.py
+++ todo_list/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import List
 
+#creating a "ListForm" from django's models.List it has an item, our to-do list item such as take out garbage, or finish project, and completed, ...
+#a Boolean that allows the program to know if an item is "completed" or not
 class ListForm(forms.ModelForm):
     class Meta:
         model = List
